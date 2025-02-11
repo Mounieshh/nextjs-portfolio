6b0c89dd29979f416de573a26a73aab2b6ec30e6
Mouniesh <iammounieshv@gmail.com>
2025-02-11 13:49:47 +0530

    navbar done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link"
 import { useState } from "react";
+import NavLink from "./navlink";
 
 
 const links = [
@@ -19,25 +20,27 @@ const NavBar = () => {
 
   return (
     <>
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-lg">
 
-        <div className="hidden md:flex gap-8">
+        {/* LINKS */}
+        <div className="hidden md:flex gap-8 w-1/3">
         {links.map((link) => (
-          <Link href={link.url} key={link.title}>{link.title}</Link>
+          <NavLink link={link} key={link.title}/>
         ))}
 
         </div>
 
-        {/* Small Screens */}
         {/* LOGO */}
-        <div className="md:hidden">
+        <div className="md:hidden lg:flex w-1/3 justify-center">
           <Link href='/' className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
           <span className="text-white mr-1">Lama</span>
           <span className="text-black bg-white rounded-md w-12 h-8 flex items-center justify-center">.dev</span>
           </Link>
         </div>
         
-        <div>
+
+        {/* SOCIAL */}
+        <div className="hidden md:flex gap-4 w-1/3">
         <Link href="/">
           <Image src="/github.png" alt="" width={24} height={24} />
         </Link>
@@ -58,6 +61,7 @@ const NavBar = () => {
         </Link>
         </div>
 
+        {/* HAMBURGER MENU */}
         <div className="md:hidden">
           <button className="w-10 h-10 flex flex-col justify-between z-50 relative" onClick={() => setOpen((prev) => !prev)}>
             <div className="w-10 h-1 bg-white rounded"></div>
---
+++ src/components/navlink.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+const NavLink = ({link}) => {
+
+    const pathname = usePathname();
+  return (
+    <>
+    <div>
+        <Link href={link.url} className={`rounded p-2 ${pathname === link.url && "bg-black text-white"}`}>
+        {link.title}
+        </Link>
+    </div>
+    </>
+  )
+}
+
+export default NavLink
